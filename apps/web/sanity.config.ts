c75fbad1c71c74c0871c23d6d62e1800fdeeef53
Claudio Rivera <me@claudiorivera.com>
2023-04-09 19:48:11 -0500

    🚚 Move schemas into web

--- apps/web/sanity.config.ts
+++ apps/web/sanity.config.ts
@@ -1,7 +1,8 @@
 import { visionTool } from "@sanity/vision";
 import { defineConfig } from "sanity";
 import { deskTool } from "sanity/desk";
-import { schemaTypes } from "@acme/schemas";
+
+import { schemaTypes } from "~/schemas";
 
 const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID;
 const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET;
